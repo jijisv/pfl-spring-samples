ed3f795c405ca5e8adbf99eaec02aed7da486f6c
jijisv <jijisv@gmail.com>
2012-10-05 19:42:05 +0000

    c-namespace & p-namespace

---
+++ spring-samples/spring-samples-cpns/src/test/java/com/pfl/samples/spring/cpns/PersonTest.java
@@ -0,0 +1,36 @@
+/**
+ * PersonTest.java
+ * 
+ * http://programmingforliving.blogspot.com
+ */
+package com.pfl.samples.spring.cpns;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Jiji_Sasidharan
+ *
+ */
+public class PersonTest {
+	
+	@Test
+	public void test() throws Exception {
+		try {
+			ApplicationContext ctx = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+			Person personBean1 = ctx.getBean("person1", Person.class);
+			Assert.assertEquals(personBean1.getFirstName(), "John");
+			Assert.assertEquals(personBean1.getAddress().getHouseNo(), "123");
+			
+			Person personBean2 = ctx.getBean("person2", Person.class);
+			Assert.assertEquals(personBean2.getFirstName(), "Joe");
+			Assert.assertEquals(personBean2.getAddress().getHouseNo(), "321");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
